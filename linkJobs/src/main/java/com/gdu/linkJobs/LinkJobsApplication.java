c8bd9ed4c9957e847b99200623ee5fbeba54b5bd
GDJ26 <GDJ26@DESKTOP-0B7G2R0>
2020-06-24 15:53:26 +0900

    [최지선] LinkJobs 파일 생성

---
+++ linkJobs/src/main/java/com/gdu/linkJobs/LinkJobsApplication.java
@@ -0,0 +1,13 @@
+package com.gdu.linkJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkJobsApplication.class, args);
+	}
+
+}
---
+++ linkJobs/src/test/java/com/gdu/linkJobs/LinkJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.linkJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
