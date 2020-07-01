1dcf35d52105b083e7dd08cf738d859ceb98ac63
gd <gd@DESKTOP-9OI7EO3>
2020-07-01 17:27:09 +0900

    [김서영, 김민정] 지역 리스트 출력 완성 1. addMember.html 지역 리스트 출력 완성 2. AreaRestController 수정 3. AreaMapper sql수정

--- linkJobs/src/main/java/com/gdu/linkJobs/restController/AreaRestController.java
+++ linkJobs/src/main/java/com/gdu/linkJobs/restController/AreaRestController.java
@@ -21,11 +21,8 @@ public class AreaRestController {
 	
 	// 군구
 	@PostMapping("/selectArea2")
-	public List<AreaAndArea2> selectArea2(Model model, HttpSession session, @RequestParam("areaNo") int areaNo) {
+	public List<AreaAndArea2> selectArea2( HttpSession session, @RequestParam("areaNo") int areaNo) {
 		System.out.println(areaNo+"<--areaNo");
-		List<AreaAndArea2> area2List=areaService.getArea2(areaNo);
-		System.out.println(area2List);
-		model.addAttribute("area2List", area2List);
-		return area2List;
+		return areaService.getArea2(areaNo);
 	}
 }  
